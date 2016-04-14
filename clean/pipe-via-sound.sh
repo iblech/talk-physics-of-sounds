8c4d456774698e590cbace38589f25c57bbb5d13
Ingo Blechschmidt <iblech@web.de>
2016-04-15 00:23:04 +0200

    Optionally limit the recording duration

--- clean/pipe-via-sound.sh
+++ clean/pipe-via-sound.sh
@@ -8,8 +8,13 @@
 #     ./generate-sine 8000 440 | ./pipe-via-sound.sh 8000 44100 | ./print-spectrum 44100
 #
 # The first parameter specifies the input bitrate, the second the output
-# bitrate.
+# bitrate. Both default to 44100. An optional third parameter specifies how
+# many seconds to record. If it isn't given, no limit is imposed.
 
 aplay -f S32_LE -r${1:-44100} < /dev/stdin &
 
-arecord -f S32_LE -r ${2:-44100} -t raw
+if [ -n "$3" ]; then
+    arecord -f S32_LE -r ${2:-44100} -t raw -d "$3"
+else
+    arecord -f S32_LE -r ${2:-44100} -t raw
+fi
