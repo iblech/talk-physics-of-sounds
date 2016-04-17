38f63f188a926805f31c2a29d5a1c8d74a52c3c1
Ingo Blechschmidt <iblech@web.de>
2016-04-17 22:44:12 +0200

    Fix echo demo

--- clean/demo-echo.sh
+++ clean/demo-echo.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-{
-    dd if=/dev/zero bs=4 count=$((44100*2));
-    arecord -t raw -fS32_LE -r44100;
-} | aplay -fS32_LE -r44100
+delay=${1:-2}
+bitrate=${2:-8000}
+
+arecord -t raw -fU8 -r$bitrate | {
+    sleep $delay
+    aplay -fU8 -r$bitrate
+}
+
+# The low quality settings are needed such that the kernel pipe buffer is not
+# exceeded. This would block arecord from further recording. A more robust
+# alternative is the following instead of sleep $delay:
+#
+#     dd count=1 bs=$((bitrate*delay)) | perl -we 'local $/; my $data = <>; print $data'
